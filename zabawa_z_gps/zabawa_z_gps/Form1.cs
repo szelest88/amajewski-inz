6472ec5b6ec83356d175f57fa52dfcc761395dba
szelest88 <szelest88@1af42b88-3941-b471-f6da-05e4f7be1b91>
2010-08-26 17:48:44 +0000

    Initial import.

---
+++ zabawa_z_gps/zabawa_z_gps/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows;
+using System.Resources;
+
+
+namespace zabawa_z_gps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            Serialport port = new SerialPort("COM12");
+            port.BaudRate = 38400;
+            port.DataBits = 8;
+            port.Parity = Parity.None;
+            port.StopBits = StopBits.One;
+            try
+            {
+                port.Open();
+            }
+            catch (Exception ex)
+            {
+                DataStream datastr = "Exception: " + ex.Message + " " + ex.StackTrace;
+            }
+        }
+    }
+}
