97ff82b7a6d94e7d6d47ff60f4ec816c0ef8a1c4
szelest88 <szelest88@1af42b88-3941-b471-f6da-05e4f7be1b91>
2010-08-26 22:02:12 +0000

    initial import of WordLearning

---
+++ trunk/WordLearning/WordLearning/RzadBoxow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WordLearning
+{
+    public partial class RzadBoxow
+    {
+        public List<CheckBox> lista;
+        CheckBox temp;
+        public RzadBoxow(int ile, int x, int y)
+        {
+            lista = new List<CheckBox>();
+            for (int i = 0; i < ile; i++)
+            {
+                temp = new CheckBox();
+                temp.Width = 15;
+                temp.Text = "a";
+                
+                temp.Left = x+i*90;
+                
+                temp.Top = y;
+                
+                lista.Add(temp);
+            }
+            
+        }
+        public void rozszerzRzad()
+        {
+            temp = new CheckBox();
+            temp.Width = 15;
+            temp.Text = "a";
+
+            temp.Left = 0;
+
+            temp.Top = 0;
+
+            lista.Add(temp);
+            
+        }
+        
+
+    }
+}
