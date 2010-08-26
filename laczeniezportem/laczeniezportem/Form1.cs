3c4f3954648dd4160b15f6a7d7717a29f1362b58
szelest88 <szelest88@1af42b88-3941-b471-f6da-05e4f7be1b91>
2010-08-26 21:55:55 +0000

    Initial do portu.

---
+++ laczeniezportem/laczeniezportem/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void click(object sender, EventArgs e)
+        {
+            if(!timer1.Enabled)
+                timer1.Enabled = true;
+        }
+
+        private void tick(object sender, EventArgs e)
+        {
+            String exception = "";
+            String to_append = "";
+            to_append = GPSConnector.GetData("COM11", exception);//12?
+            to_append.Trim();
+            if (to_append.Length>2)
+            {
+                textBox1.AppendText(to_append);
+            }
+            
+                if(exception!="")
+            label1.Text = "Nastąpilł wyjątek: " + exception;
+     
+        }
+    }
+}
---
+++ laczeniezportem/laczeniezportem/GPSConnector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO.Ports;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class GPSConnector
+    {
+
+        public static String GetData(String port_name, String exception)
+        {
+            String temp = "" ;
+            SerialPort port = new SerialPort(port_name);
+            port.BaudRate = 38400;
+            port.DataBits = 8;
+            port.Parity = Parity.None;
+            port.StopBits = StopBits.One;
+            exception = "";
+            
+            try
+            {
+                port.Open();
+                temp = port.ReadLine();
+                port.Close();
+            }
+            catch (Exception ex)
+            {
+                String datastr = ex.Message + ", " + ex.StackTrace;
+                exception = datastr;
+                port.Close();
+            }
+            return temp;
+        }
+
+    }
+}
