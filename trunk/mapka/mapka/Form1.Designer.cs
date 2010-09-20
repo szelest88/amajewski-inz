b1607d96e6ddd9fa8729697c5152d58b1f34ffe1
szelest88 <szelest88@1af42b88-3941-b471-f6da-05e4f7be1b91>
2010-09-20 16:24:40 +0000

    Poprawka GUI - wybór rysowanego elementu

--- trunk/mapka/mapka/Form1.Designer.cs
+++ trunk/mapka/mapka/Form1.Designer.cs
@@ -33,7 +33,9 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.radioButtonPunkty = new System.Windows.Forms.RadioButton();
+            this.radioButtonLinie = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -81,23 +83,49 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // checkBox1
+            // radioButtonPunkty
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(151, 4);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(47, 17);
-            this.checkBox1.TabIndex = 4;
-            this.checkBox1.Text = "lines";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.radioButtonPunkty.AutoSize = true;
+            this.radioButtonPunkty.Location = new System.Drawing.Point(142, 10);
+            this.radioButtonPunkty.Name = "radioButtonPunkty";
+            this.radioButtonPunkty.Size = new System.Drawing.Size(58, 17);
+            this.radioButtonPunkty.TabIndex = 5;
+            this.radioButtonPunkty.TabStop = true;
+            this.radioButtonPunkty.Text = "Punkty";
+            this.radioButtonPunkty.UseVisualStyleBackColor = true;
+            this.radioButtonPunkty.CheckedChanged += new System.EventHandler(this.radioButtonPunkty_CheckedChanged);
+            // 
+            // radioButtonLinie
+            // 
+            this.radioButtonLinie.AutoSize = true;
+            this.radioButtonLinie.Location = new System.Drawing.Point(233, 10);
+            this.radioButtonLinie.Name = "radioButtonLinie";
+            this.radioButtonLinie.Size = new System.Drawing.Size(47, 17);
+            this.radioButtonLinie.TabIndex = 6;
+            this.radioButtonLinie.TabStop = true;
+            this.radioButtonLinie.Text = "Linie";
+            this.radioButtonLinie.UseVisualStyleBackColor = true;
+            this.radioButtonLinie.CheckedChanged += new System.EventHandler(this.radioButtonLinie_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(325, 10);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(436, 294);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButtonLinie);
+            this.Controls.Add(this.radioButtonPunkty);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
@@ -124,7 +152,9 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton radioButtonPunkty;
+        private System.Windows.Forms.RadioButton radioButtonLinie;
+        private System.Windows.Forms.RadioButton radioButton3;
     }
 }
 
